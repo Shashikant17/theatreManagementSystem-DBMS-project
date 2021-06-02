ae6fbd0a957e71024756b7d3078ec62b46ba3f16
uditagarwal1305 <55307669+uditagarwal1305@users.noreply.github.com>
2021-06-03 01:24:27 +0530

    final project

---
+++ whole final project/mail.py
@@ -0,0 +1,52 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# '''
+# username = "Shashi Kant"
+# movie_name = "Mission Impossible"
+# hall_name = "starplex"
+# show_time = "03:15:00"
+# price = 110
+
+# receiver_address = "agarwaludit638@gmail.com"
+# '''
+
+def mail(username,movie_name,hall_name,theatre_location,show_time,price,receiver_address,seat_name,date):
+    sender_address = "moviesvoom@gmail.com"
+    sender_app_pass = "Moviesvoom&com"
+    
+    body ="""Dear {},
+                Thank You for booking Movie 🎫Ticket from MoviesVoom.com
+                Your Ticket Details are as follow -
+                Movie Name = {} 🎬
+                Theatre Name = {}
+                Theatre Loaction = {}
+                Show Time = {}
+                Total Price = {}
+                Comfort Zone= {}
+                Billing Date= {}
+    
+    Hope you enjoy.😊😊
+    
+    Thank You
+    MoviesVoom!!!
+
+    Note: Please Collect your seat Number from Counter!!!
+    """.format(username,movie_name,hall_name,theatre_location,show_time,price,seat_name,date)
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_address
+    msg['To'] = receiver_address
+    msg['Subject'] = "Ticket confirmation by MoviesVoom"
+    msg.attach(MIMEText(body, 'plain'))
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(sender_address, sender_app_pass)
+    body = msg.as_string()
+    s.sendmail(sender_address, receiver_address, body)
+    s.quit()
+    return("Mail Sent!!!")
+
+#mail(username,movie_name,hall_name,show_time,price,receiver_address)
